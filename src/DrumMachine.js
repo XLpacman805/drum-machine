d61e0d8a85e9b1d6c26ef425b1dc02f64f04d7d6
Johnny Meza <xlpacman805@gmail.com>
2020-04-11 21:48:52 -0700

    Created drum machine component

---
+++ src/DrumMachine.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class DrumMachine extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return ( 
+            <h2>Drum Machine Thing</h2>
+         );
+    }
+}
+
+export default DrumMachine;
