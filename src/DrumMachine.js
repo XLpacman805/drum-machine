a6173e0ec4ac8c7afef83826a942167de0cc54c1
Johnny Meza <xlpacman805@gmail.com>
2020-04-17 16:34:29 -0700

    Created 808 soundpack

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -15,16 +15,16 @@ class DrumMachine extends React.Component {
                 <h2>Drumming Machine World</h2>
                 <div className="grid-container">
                     <Drumpad className="grid-item-one" text="Q" audioPath={EightZeroEightDrumKit[0]} />
-                    <Drumpad className="grid-item-two" text="W" audioPath={EightZeroEightDrumKit[0]} />
-                    <Drumpad className="grid-item-three" text="E" />
+                    <Drumpad className="grid-item-two" text="W" audioPath={EightZeroEightDrumKit[1]} />
+                    <Drumpad className="grid-item-three" text="E" audioPath={EightZeroEightDrumKit[2]}/>
 
-                    <Drumpad className="grid-item-four" text="A" />
-                    <Drumpad className="grid-item-five" text="S" />
-                    <Drumpad className="grid-item-six" text="D" />
+                    <Drumpad className="grid-item-four" text="A" audioPath={EightZeroEightDrumKit[3]} />
+                    <Drumpad className="grid-item-five" text="S" audioPath={EightZeroEightDrumKit[4]} />
+                    <Drumpad className="grid-item-six" text="D" audioPath={EightZeroEightDrumKit[5]} />
 
-                    <Drumpad className="grid-item-seven" text="Z" />
-                    <Drumpad className="grid-item-eight" text="X" />
-                    <Drumpad className="grid-item-nine" text="C" />
+                    <Drumpad className="grid-item-seven" text="Z" audioPath={EightZeroEightDrumKit[6]} />
+                    <Drumpad className="grid-item-eight" text="X" audioPath={EightZeroEightDrumKit[7]} />
+                    <Drumpad className="grid-item-nine" text="C" audioPath={EightZeroEightDrumKit[8]} />
                 </div>
             </div>
          );
